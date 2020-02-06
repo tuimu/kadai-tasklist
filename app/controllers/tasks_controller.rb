5ee285e681f4efda2573131bcddb333043712b17
Mao.Taro <tuimu4147@gmail.com>
2020-02-06 14:54:26 +0000

    CRUD Tasks todo

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,57 @@
+class TasksController < ApplicationController
+    
+    def index
+        @tasks = Task.all
+    end
+    
+    def show
+        @task = Task.find(params[:id])
+    end
+    
+    def new
+        @task = Task.new(todo: 'preset task')
+    end
+    
+    def create
+        @task = Task.new(task_params)
+        
+        if @task.save
+            flash[:success] = 'Todo is created!'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'Todo is missed...'
+            render :new
+        end
+    end
+    
+    def edit
+        @task = Task.find(params[:id])
+    end
+    
+    def update
+        @task = Task.find(params[:id])
+        
+        if @task.save
+            flash[:success] = 'Todo is updated!'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'Todo is remained...'
+            render :edit
+        end
+    end
+    
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        
+        flash[:success] = 'Todo is destroyed!'
+        redirect_to tasks_url
+    end
+    
+    private
+    
+    def task_params
+        params.require(:task).permit(:todo)
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,33 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  root to: 'tasks#index'
+  resources :tasks
+  
+=begin
+  get 'tasks/:id', to: 'tasks#show'
+  post 'tasks/', to: 'tasks#create'
+  put 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
+  
+  get 'tasks/', to: 'tasks#index'
+  
+  get 'tasks/new', to: 'tasks#new'
+  
+  get 'tasks/:id/edit', to: 'tasks#edit'
+=end
+
+=begin
+   Prefix Verb   URI Pattern                     Controller#Action
+     root GET    /                               tasks#index
+    tasks GET    /tasks(.:format)                tasks#index
+          POST   /tasks(.:format)                tasks#create
+ new_task GET    /tasks/new(.:format)            tasks#new
+edit_task GET    /tasks/:id/edit(.:format)       tasks#edit
+     task GET    /tasks/:id(.:format)            tasks#show
+          PATCH  /tasks/:id(.:format)            tasks#update
+          PUT    /tasks/:id(.:format)            tasks#update
+          DELETE /tasks/:id(.:format)            tasks#destroy
+=end
+  
 end
